45426f283c3a624bc0287a6609fd88081a4a9456
gurinowicz <tkachukya@gmail.com>
2024-02-20 21:40:00 +0300

    The first HW

---
+++ Homework1.py
@@ -0,0 +1,23 @@
+# 1
+print('Hello,world!')
+# 2
+user_name = str(input('Write your name'))
+print('Hello, ' + user_name)
+# 3
+a = 24
+b = 23
+print('Result = ' + str(a*b))
+# 4
+print('********\n*      *\n*      *\n********')
+# 5
+c = input('Разложить число:  ')
+print('Тысячи - '+ c[0])
+print('Cотни- '+ c[1])
+print('Десятки - '+ c[2])
+print('Единицы - '+ c[3])
+# 6
+a = int(input())
+b = int(input())
+# 7
+print('Квадрат суммы:' +str(a) + ' ' + 'и' + ' ' +str(b)+' ' +'равен'+' ' +str((a+b)**2))
+print('Сумма квадратов:' +str(a) + ' '+ 'и' + ' ' +str(b) +' ' +'равен'+' ' +str((a**2+b**2)))
